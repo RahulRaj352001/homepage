e543669f78736c3457643ac9cd4c1fce38da7df4
Rahul Rajput <RahulRaj352001@outlook.com>
2021-09-10 02:29:08 +0530

    basics done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Header from './components/Header'
 import Workout from './components/Workout'
 import Craft from './components/Craft'
 import Featured from './components/Featured';
+import Company from './components/Company';
+import Grid from './components/Grid';
+import Colaborate from './components/Colaborate';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
@@ -13,6 +17,10 @@ function App() {
       </div>
        <Craft/>
        <Featured/>
+       <Company/>
+       <Grid/>
+       <Colaborate/>
+       <Footer/>
     </div>
   );
 }
---
+++ src/components/Grid.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+export default function Grid() {
+    return (
+        <div>
+            <div class="c-grid">
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/Google.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/NYT.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/pbs-logo.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/Insider_Inc.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/BMI.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/apm.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/fort-point.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/Hyatt_Logo.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/texas-de-brazil.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/marketplace.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/liquidnet.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/mooyah.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/Hi-Rez.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/ltn.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/hu-kitchen.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/AIHA_logo.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/smith.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/harvard.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/netwerk.svg" class="b-clients__logo lozad"/>
+        </div>
+            <div class="b-clients__item">
+            <img src="https://s3.us-east-1.amazonaws.com/paper-tiger-website/uploads/new-home-logos/ID2020png.svg" class="b-clients__logo lozad"/>
+        </div>
+    
+
+<div>  </div>
+</div>
+        </div>
+    )
+}
